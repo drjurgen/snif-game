e075ed7d9dfdfbf17d28e5cf3d5253b04fd3db5c
NicolaiHJørgensen <nicoj98@gmail.com>
2020-10-14 23:08:15 +0200

    implemented bathroom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", loadSVG);
+window.addEventListener("DOMContentLoaded", setupGame);
 
 let svg;
 
@@ -39,12 +39,26 @@ let walkingToLeftLight = false;
 let walkingToRightLight = false;
 
 let livingroomListeners = false;
+let bathroomListeners = false;
+
+// BATHROOM PATH
+let bathroomPath = `path("M250.9,440.3c0,0,102.3-25.4,128.8-69.8S247.1,237.6,247.1,237.6S189,250.9,199.9,277s154.2,82.1,139.9,93.5 c-27.6,22.2-59.3,47.7-92.4,48.5c-47.8,1.2-166.1-39.8-124.3-84.3c16.7-17.7-38,26.6-38,26.6s-10.5,2,10.5,17.8 C108.6,399.2,192.8,436.7,250.9,440.3z")`;
+
+// WASHING MACHINE
+let walkingToWashing = false;
+
+// SHOWER
+let walkingToShower = false;
+
+// WASHING MACHINE
+let walkingToSink = false;
 
 // GAME MECHANICS
 let score = 0;
 const timer = {
   timeCounter: 60,
-  timeLeft: 600,
+  timeLeft: 60,
+  totalTimeScore: "",
 };
 
 // ENERGY SOURCE PROTOTYPE
@@ -56,14 +70,54 @@ const LivingRoomEnergySource = {
 // ARRAY FOR ALL ENERGY SOURCES IN LIVING ROOM
 const allLivingRoomSources = [];
 
-function loadSVG() {
-  console.log("load the SVG");
+// CURRENT PLAYING LEVEL
+let currentLevel = "livingroom";
+
+// SETUP GAME
+function setupGame() {
+  document.querySelector("#start-button").addEventListener("click", startGame);
+  document.querySelector("#game").style.display = "none";
+}
+
+// STATRT THE GAME
+function startGame() {
+  document.querySelector("#start-screen").style.display = "none";
+  document.querySelector("#game").style.display = "flex";
+  if (currentLevel === "livingroom") {
+    loadLivingRoom();
+  } else if (currentLevel === "bathroom") {
+    showBathroom();
+  }
+}
+
+// load livingroom svg
+function loadLivingRoom() {
+  console.log("load livingroom");
   fetch("livingroom_done.svg")
     .then((response) => response.text())
     .then((svgData) => {
-      console.log("SVG loaded");
+      console.log("livingroom SVG loaded");
       document.querySelector("#living-room").innerHTML = svgData;
       document.querySelector("#living-room").classList.add("fade-in");
+
+      // document.querySelector("#living-room").addEventListener("animationend", () => {
+      //   document.querySelector("#living-room").classList.remove("fade-in");
+      // });
+
+      currentLevel = "livingroom";
+      loadSnif();
+    });
+}
+
+// load bathroom svg
+function loadBathroom() {
+  console.log("load bathroom");
+  fetch("bathroom_snif.svg")
+    .then((response) => response.text())
+    .then((svgData) => {
+      console.log("bathroom SVG loaded");
+      document.querySelector("#bathroom").innerHTML = svgData;
+      document.querySelector("#bathroom").classList.add("fade-in");
 
       loadSnif();
     });
@@ -81,10 +135,58 @@ function loadSnif() {
       //document.querySelector("#snif_back_right").style.display = "none";
       document.querySelector("#snif_front_left").style.display = "none";
       document.querySelector("#snif_back_left").style.display = "none";
-      prepareLivingroom();
+
+      if (currentLevel === "livingroom") {
+        prepareLivingroom();
+        randomizeLivingroom(); // randomize states of energy sources
+        runLoop(); // run the game loop
+      } else if (currentLevel === "bathroom") {
+        prepareBathroom();
+        randomizeBathroom(); // randomize states of energy sources
+        if (livingroomListeners === true) {
+        } else if (livingroomListeners === false) {
+          runLoop(); // run the game loop
+        }
+
+        playBathRoom();
+      }
     });
 }
 
+function runLoop() {
+  if (currentLevel === "livingroom") {
+    if (document.querySelector("#living-room #snif").style.animationPlayState === "paused" || timer.timeLeft === 0) {
+    } else {
+      playLivingRoom();
+    }
+  } else if (currentLevel === "bathroom") {
+    if (document.querySelector("#bathroom #snif").style.animationPlayState === "paused" || timer.timeLeft === 0) {
+    } else {
+      playBathRoom();
+    }
+  }
+
+  // check time left
+  if (timer.timeCounter > 0) {
+    timer.timeCounter--;
+    if (timer.timeCounter === 0) {
+      // console.log("one second passed");
+      timer.timeCounter = 60;
+      if (timer.timeLeft > 0) {
+        timer.timeLeft--;
+        showTimeLeft();
+      }
+    }
+  }
+
+  if (timer.timeLeft > 0) {
+    requestAnimationFrame(runLoop);
+  } else {
+    endGame();
+  }
+}
+
+// LIVING ROOM LEVEL
 function prepareLivingroom() {
   // adding animation classes to objects and snif
   document.querySelector("#living-room #snif").classList.add("follow-path");
@@ -93,8 +195,8 @@ function prepareLivingroom() {
   document.querySelector("#hidden-snif #snif_front_left").classList.add("walk");
   document.querySelector("#hidden-snif #snif_back_left").classList.add("walk");
 
-  randomizeLivingroom(); // randomize states of energy sources
-  runLoop(); // run the game loop
+  // randomizeLivingroom(); // randomize states of energy sources
+  // runLoop(); // run the game loop
 }
 
 function randomizeLivingroom() {
@@ -220,32 +322,6 @@ function animateSpeaker(energyObj) {
   } else {
     document.querySelectorAll(`#${energyObj.id} #music_x5F_note_x5F_r_${noteId}`).forEach((g) => g.classList.toggle("hide"));
     document.querySelector(`#${energyObj.id}_upper`).classList.remove("scale2");
-  }
-}
-
-function runLoop() {
-  if (document.querySelector("#snif").style.animationPlayState === "paused" || timer.timeLeft === 0) {
-  } else {
-    playLivingRoom();
-  }
-
-  // check time left
-  if (timer.timeCounter > 0) {
-    timer.timeCounter--;
-    if (timer.timeCounter === 0) {
-      // console.log("one second passed");
-      timer.timeCounter = 60;
-      if (timer.timeLeft > 0) {
-        timer.timeLeft--;
-        showTimeLeft();
-      }
-    }
-  }
-
-  if (timer.timeLeft > 0) {
-    requestAnimationFrame(runLoop);
-  } else {
-    endGame();
   }
 }
 
@@ -725,17 +801,233 @@ function playLivingRoom() {
   function checkLivingroomState() {
     if (allLivingRoomSources.every((obj) => obj.isTurnedOn === false)) {
       console.log("room complete!");
+      hideLivingRoom();
+    }
+  }
 
+  function hideLivingRoom() {
+    setTimeout(function () {
+      document.querySelector("#living-room").style.animationDirection = "reverse";
+      document.querySelector("#living-room").classList.remove("fade-in");
+      void document.querySelector("#living-room").offsetHeight;
+      document.querySelector("#living-room").classList.add("fade-in");
+      document.querySelector("#living-room").addEventListener("animationend", () => {
+        document.querySelector("#living-room").classList.add("hide");
+      });
+
+      setTimeout(showBathroom, 1500);
+    }, 1000);
+  }
+}
+
+// BATHROOM LEVEL
+function showBathroom() {
+  currentLevel = "bathroom";
+  if (currentLevel === "livingroom") {
+    setTimeout(loadBathroom, 1000);
+  } else if (currentLevel === "bathroom") {
+    loadBathroom();
+  }
+}
+
+function prepareBathroom() {
+  // make snif follow the path
+  document.querySelector("#bathroom #snif").classList.add("follow-path");
+  document.querySelector("#bathroom #snif").style.offsetPath = bathroomPath;
+  document.querySelector("#hidden-snif #snif_front_right").classList.add("walk");
+  document.querySelector("#hidden-snif #snif_back_right").classList.add("walk");
+  document.querySelector("#hidden-snif #snif_front_left").classList.add("walk");
+  document.querySelector("#hidden-snif #snif_back_left").classList.add("walk");
+}
+
+function randomizeBathroom() {
+  // idk fammmm
+}
+
+function playBathRoom() {
+  if (bathroomListeners === false) {
+    bathroomListeners = true;
+
+    addGuiEvents();
+    function addGuiEvents() {
+      // add eventlisteners
+      document.querySelector("#washer_2_").addEventListener("click", goToWashing);
+      document.querySelector("#shower").addEventListener("click", goToShower);
+      document.querySelector("#sink_2_").addEventListener("click", goToSink);
+    }
+
+    setTimeout(function () {
+      endGame();
+    }, 7500);
+  }
+
+  findSnifLocation();
+  checkSnifLocation();
+
+  function checkSnifLocation() {
+    // sink, washing machine, shower
+    if (walkingToSink === true) {
+      inFrontSink();
+    }
+
+    if (walkingToWashing === true) {
+      inFrontWashing();
+    }
+
+    if (walkingToShower === true) {
+      inFrontShower();
+    }
+  }
+
+  // FIND SNIF COORDINATES
+  function findSnifLocation() {
+    svg = document.querySelector("#bathroom svg");
+
+    calcSnifCoordinates();
+    function calcSnifCoordinates() {
+      //snif vector coordinates
+      snif = document.querySelector("#bathroom #snif");
+      const percent = snif.computedStyleMap().get("offset-distance").value;
+
+      // find coordinates from the path
+      const path = document.querySelector("#bathroom #path");
+      const length = path.getTotalLength();
+      const positionOnPath = (length * percent) / 100;
+      const point = path.getPointAtLength(positionOnPath);
+      snifX = Math.floor(point.x);
+      snifY = Math.floor(point.y);
+
+      chooseSnifDirection(positionOnPath);
+    }
+
+    // console.log({ snifX, snifY });
+  }
+
+  // WASHING MACHINE
+  function goToWashing() {
+    walkingToWashing = true;
+    document.querySelectorAll(".walk").forEach((sprite) => (sprite.style.animationPlayState = "running")); //start all walk animations on all snif-sprites
+    document.querySelector("#bathroom #snif").style.animationPlayState = "running"; //run path animation
+  }
+
+  function inFrontWashing() {
+    if (snifX > 370 && snifX < 380 && snifY > 370 && snifY < 380) {
+      document.querySelector("#bathroom #snif").style.animationPlayState = "paused"; //pause path animation
+      stopWalkAnimation();
+      walkingToWashing = false;
+    }
+  }
+
+  // WASHING MACHINE
+  function goToShower() {
+    walkingToShower = true;
+    document.querySelectorAll(".walk").forEach((sprite) => (sprite.style.animationPlayState = "running")); //start all walk animations on all snif-sprites
+    document.querySelector("#bathroom #snif").style.animationPlayState = "running"; //run path animation
+  }
+
+  function inFrontShower() {
+    if (snifX > 205 && snifX < 225 && snifY > 240 && snifY < 255) {
+      document.querySelector("#bathroom #snif").style.animationPlayState = "paused"; //pause path animation
+      stopWalkAnimation();
+      walkingToShower = false;
+    }
+  }
+
+  // WASHING MACHINE
+  function goToSink() {
+    walkingToSink = true;
+    document.querySelectorAll(".walk").forEach((sprite) => (sprite.style.animationPlayState = "running")); //start all walk animations on all snif-sprites
+    document.querySelector("#bathroom #snif").style.animationPlayState = "running"; //run path animation
+  }
+
+  function inFrontSink() {
+    if (snifX > 115 && snifX < 125 && snifY > 325 && snifY < 340) {
+      document.querySelector("#bathroom #snif").style.animationPlayState = "paused"; //pause path animation
+      stopWalkAnimation();
+      walkingToSink = false;
+    }
+  }
+
+  // STOP SNIF WALK ANIMATION
+  function stopWalkAnimation() {
+    //pause all walk animations on all snif-sprites
+    document.querySelectorAll("#hidden-snif .walk").forEach((sprite) => {
+      sprite.addEventListener("animationiteration", toggleWalk);
+      function toggleWalk() {
+        sprite.classList.remove("walk");
+        sprite.classList.add("walk");
+        sprite.style.animationPlayState = "paused";
+      }
       setTimeout(function () {
-        document.querySelector("#living-room").style.animationDirection = "reverse";
-        document.querySelector("#living-room").classList.remove("fade-in");
-        void document.querySelector("#living-room").offsetHeight;
-        document.querySelector("#living-room").classList.add("fade-in");
-      }, 1000);
+        sprite.removeEventListener("animationiteration", toggleWalk);
+      }, 500);
+    });
+  }
+
+  // CHOOSE CORRECT SNIF SPRITE FOR WALKING DIRECTION
+  function chooseSnifDirection(positionOnPath) {
+    //console.log(positionOnPath);
+    let snifDirection;
+
+    if (positionOnPath < 310 && positionOnPath > 200) {
+      snifDirection = "back right";
+      showSnifDirection(snifDirection);
+    } else if (positionOnPath < 210 && positionOnPath > 110) {
+      snifDirection = "front right";
+      showSnifDirection(snifDirection);
+    } else if (positionOnPath < 100) {
+      snifDirection = "front left";
+      showSnifDirection(snifDirection);
+    } else if (positionOnPath < 50) {
+      snifDirection = "back left";
+      showSnifDirection(snifDirection);
+    }
+  }
+
+  // SHOW CORRECT SNIF SPRITE FOR WALKING DIRECTION
+  function showSnifDirection(snifDirection) {
+    if (snifDirection === "back right") {
+      //console.log(snifDirection)
+      document.querySelector("#snif_back_left").style.display = "none";
+      document.querySelector("#snif_front_left").style.display = "none";
+      document.querySelector("#snif_front_right").style.display = "none";
+
+      // show correct direction
+      document.querySelector("#snif_back_right").style.display = "inline";
+    } else if (snifDirection === "front right") {
+      //console.log(snifDirection)
+      document.querySelector("#snif_back_right").style.display = "none";
+      document.querySelector("#snif_back_left").style.display = "none";
+      document.querySelector("#snif_front_left").style.display = "none";
+
+      // show correct direction
+      document.querySelector("#snif_front_right").style.display = "inline";
+    } else if (snifDirection === "back left") {
+      //console.log(snifDirection)
+      document.querySelector("#snif_back_right").style.display = "none";
+      document.querySelector("#snif_front_left").style.display = "none";
+      document.querySelector("#snif_front_right").style.display = "none";
+
+      // show correct direction
+      document.querySelector("#snif_back_left").style.display = "inline";
+    } else if (snifDirection === "front left") {
+      //console.log(snifDirection)
+      document.querySelector("#snif_back_right").style.display = "none";
+      document.querySelector("#snif_back_left").style.display = "none";
+      document.querySelector("#snif_front_right").style.display = "none";
+
+      // show correct direction
+      document.querySelector("#snif_front_left").style.display = "inline";
     }
   }
 }
 
+//
+//
+//
+//
+//
+// GAME MECHANICS
 function increaseScore() {
   score++;
   document.querySelector(".score").textContent = `score: ${score}`;
@@ -747,8 +1039,20 @@ function showTimeLeft() {
 
 function endGame() {
   console.log("game finished");
-  document.querySelector("#living-room").style.animationDirection = "reverse";
-  document.querySelector("#living-room").classList.remove("fade-in");
-  void document.querySelector("#living-room").offsetHeight;
-  document.querySelector("#living-room").classList.add("fade-in");
+  document.querySelector("#game").style.animationDirection = "reverse";
+  document.querySelector("#game").classList.remove("fade-in");
+  void document.querySelector("#game").offsetHeight;
+  document.querySelector("#game").classList.add("fade-in");
+  document.querySelector("#game").addEventListener("animationend", () => {
+    document.querySelector("#game").style.display = "none";
+    document.querySelector("#end-screen").style.display = "flex";
+  });
+
+  timer.totalTimeScore = -timer.timeLeft + 60;
+
+  const timeScore = document.createElement("h1");
+  timeScore.textContent = `you finished the game in: ${timer.totalTimeScore} seconds!`;
+
+  document.querySelector("#end-screen").appendChild(timeScore);
+  console.log(`You finished the game in ${timer.totalTimeScore} seconds!`);
 }
